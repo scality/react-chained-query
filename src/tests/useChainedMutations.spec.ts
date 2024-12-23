25c86092c6c527807cd7c59103791cc29e7b1276
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2024-12-23 15:05:00 +0100

    Bump react to 18

--- src/tests/useChainedMutations.spec.ts
+++ src/tests/useChainedMutations.spec.ts
@@ -1,4 +1,4 @@
-import { act, renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react';
 import { useChainedMutations } from '../useChainedMutations';
 
 const fn1 = jest.fn();
